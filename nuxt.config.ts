34943a47c3f9a0b5e2765327d077c84b361e2821
Dominik Fehér <feherdominik97@gmail.com>
2024-08-04 22:36:03 +0200

    remove unused config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/scss/main.scss'],
-  build:{
-    transpile: [/@vueuse\/nuxt/]
-  },
   components: [
     {
       path: "~/components"
